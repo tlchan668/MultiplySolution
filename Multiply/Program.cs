a59f99a0c1168a7b77e66d65f0a602a0917f2fbb
Traci Chan <tlc668@gmail.com>
2020-02-09 14:24:56 -0500

    initial commit multiply

---
+++ Multiply/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Multiply {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Multiply");
+            var sum = 1;
+            for (var index = 1; index<11; index++) {
+                sum = sum*index;
+                //Console.WriteLine(index);
+                }
+            Console.WriteLine($"Sum is {sum}");//interpolation way to build strings
+            Console.WriteLine(sum);
+
+
+            //alternate solution
+            var product = 1;
+            var nbr = 1;
+            while (nbr <= 10) {
+                product *= nbr;
+                nbr++;
+                }
+            Console.WriteLine($"Product is {product}");
+            }
+        }
+    }
